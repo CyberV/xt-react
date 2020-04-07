352182e905d19481cd6ead889910ad154d5eaeab
Vikrant Siwach <viksiwac@publicisgroupe.net>
2020-04-08 02:26:41 +0530

    Adding Bootstrap and basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import CharacterCard from './components/characterCard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Yo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="main-body container">
+        <div className="row avlbl-filters">
+          <div className="col-xs-12 col-md-3"> Available Filters</div>
+          <div className="col-xs-12 col-md-9">
+            <div className="row">
+              <div className="col-sm-12 slctd-filters">Selected Filters</div>
+              <div className="col-sm-12 char-list">Char List
+                <CharacterCard></CharacterCard>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
       </header>
+
     </div>
   );
 }
---
+++ src/components/characterCard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class CharacterCard extends Component {
+  render() {
+     return (
+     <div>
+        <span>Yo Char yo</span>
+    </div>
+    
+    );
+  }
+}
+export default CharacterCard; 
